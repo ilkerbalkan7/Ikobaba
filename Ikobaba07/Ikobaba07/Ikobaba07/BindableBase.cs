3067b9b91b95691f3d5ac68062925a1dca3e8bbb
ilkerbalkan7 <119609512+ilkerbalkan7@users.noreply.github.com>
2022-12-16 18:03:15 +0300

    Add files via upload

---
+++ Ikobaba07/Ikobaba07/Ikobaba07/BindableBase.cs
@@ -0,0 +1,37 @@
+﻿using Ikobaba07.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikobaba07  
+{
+    class BindableBase : INotifyPropertyChanged
+    {
+
+        protected virtual void SetProperty<T>(ref T member, T val,
+           [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(member, val)) return;
+
+            member = val;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public static implicit operator BindableBase(OrderViewModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
